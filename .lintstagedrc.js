ba12d952f30206e4c13cd50a429404f483c86a9e
JerrySun <sunmingda@agora.io>
2023-04-18 14:05:29 +0800

    config: update lint-stage & vite-config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "!**/public/**": "",
-  "*.{vue,ts,tsx,js}": ["eslint --cache --fix"],
+  "*.{vue,ts,tsx}": ["eslint --cache --fix"],
   "*.json": ["prettier --parser=json --write"],
   "*.{css,scss}": ["prettier --parser=css --write"],
 };
--- packages/schema-generator/vite.config.ts
+++ packages/schema-generator/vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
+  base: "./",
   plugins: [vue(), WindiCSS()],
   css: {
     preprocessorOptions: {
