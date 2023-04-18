c4163552ab9bc5166995be1f0ae8a2326a94e30d
JerrySun <sunmingda@agora.io>
2023-04-18 13:50:38 +0800

    fix: update lintstage config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "!**/public/**": "",
   "*.{vue,ts,tsx,js}": ["eslint --cache --fix"],
   "*.json": ["prettier --parser=json --write"],
   "*.{css,scss}": ["prettier --parser=css --write"],
